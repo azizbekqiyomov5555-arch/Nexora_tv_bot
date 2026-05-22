9954128cd2d42cfe1341105bcd75127e7fda920f
azizbekqiyomov5555-arch <azizbekqiyomov5555@gmail.com>
2026-05-22 10:07:37 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ from telegram.ext import (
 
 # ─── KONFIGURATSIYA ────────────────────────────────────────
 BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8623090274:AAHAH4pk5ZPjtdcNeahA4uVFtqFcmup-WyU"
-ADMIN_ID   = int(os.environ.get("ADMIN_ID") or "8537782289")
+ADMIN_ID   = int(os.environ.get("ADMIN_ID") or "7632975179")
 
 DATABASE_URL      = os.environ.get("DATABASE_URL") or ""
 JSONBLOB_URL      = ""   # O'chirildi — PostgreSQL ishlatilmoqda
